24783397559b04df9e34ae92581c3286cf0d9a4d
OmerCanOOzdemir <omer.can.ozdemir@student.ehb.be>
2020-09-25 10:21:19 +0200

    Ajoutez des fichiers projet.

---
+++ WPO1_NET/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace WPO1_NET
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+        public static void wissel(int a,int b)
+        {
+            int temp;
+            temp = a;
+            a = b;
+            b = temp;
+        }
+    }
+}
